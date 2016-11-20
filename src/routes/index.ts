00f08fe5caf7edd2991d7f08d11629c94d4de22a
Gabriel Trabanco Llano <gtrabanco@fwok.org>
2016-11-20 19:09:01 +0000

    First release

---
+++ src/routes/index.ts
@@ -0,0 +1,28 @@
+"use strict";
+
+import * as express from "express";
+
+var geoip = require('geoip-lite');
+
+module Route {
+
+    export class Index {
+
+        public index(req: express.Request, res: express.Response, next: express.NextFunction) {
+
+            let ip = req.params.ip ? req.params.ip : (req.ip || null);
+            let data = ip?geoip.lookup(ip):{};
+
+            if(data && ip) {
+                data.ip = ip;
+            }
+
+            return res.jsonp({
+                code: (data?200:400),
+                data: data
+            });
+        }
+    }
+}
+
+export = Route;
